28c69ee05f75ee007967e05c556e38adf54e2c3e
acm-sigdoc-structured <159305507+acm-sigdoc-structured@users.noreply.github.com>
2024-02-16 09:39:20 -0500

    Uploaded ACM Quickstart files

---
+++ build-pdf.bat
@@ -0,0 +1,4 @@
+@echo on
+REM Substitute a local Windows path to your copy of DITA-OT. 
+REM Generated PDF outout is in the subfolder named .\out. 
+C:\dita-ot-4.1.2\bin\dita --input=bard_map.ditamap --format=pdf
